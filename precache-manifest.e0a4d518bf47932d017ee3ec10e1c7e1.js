a1e0235134f4e8cce3adc8d48f788064a5dfba04
Shrouk <shroukgalbat0@gmail.com>
2022-06-02 06:32:19 +0200

    Updates

---
+++ precache-manifest.e0a4d518bf47932d017ee3ec10e1c7e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bace958a484f26637f3664126d2ad1c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d5c7833eddc2addef33",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d92d7aad3bcaeb839378",
+    "url": "/static/js/2.4ee05d63.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4ee05d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5c7833eddc2addef33",
+    "url": "/static/js/main.b037c000.chunk.js"
+  },
+  {
+    "revision": "f28ebceb673f08adcd29",
+    "url": "/static/js/runtime-main.321e3172.js"
+  }
+]);
